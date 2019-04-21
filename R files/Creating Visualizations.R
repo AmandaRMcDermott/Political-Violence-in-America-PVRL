66f6c129c83685722faa628c3eba11c0a44138e3
Amanda McDermott <42814674+Glacieus@users.noreply.github.com>
2019-04-21 11:41:23 -0400

    upload

--- R files/Creating Visualizations.R
+++ R files/Creating Visualizations.R
@@ -10,6 +10,7 @@ library(gridExtra)
 library(ggthemes)
 library(usmap)
 library(kableExtra)
+library(raster)
 
 initial_pvia_2 <- read_csv("initial_pvia_2.csv")
 
@@ -34,7 +35,7 @@ coords <- read_csv("uscitiesv1.4.csv")
 US <- map_data("state")
 
 refined_coords <- coords %>%
-  select(-c(2, 3, 5, 6, 9:16)) %>% 
+  dplyr::select(city, state_name, lat, lng) %>% 
   distinct() %>% 
   rename(state = "state_name")
 
@@ -52,23 +53,60 @@ pvia_coords <- initial_pvia_2 %>%
 # Map
 ggplot() +
   geom_polygon(data = US, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3, color = "lightgray") + guides(fill = T) +
-  geom_point(data = pvia_coords %>% filter(state != "Puerto Rico"), aes(x = lng, y = lat, color = perpetrator_poli_leaning, size = fatalities), alpha = .4) +
+  geom_point(data = pvia_coords %>% filter(state != "Puerto Rico"), aes(x = lng, y = lat, color = perpetrator_class, size = fatalities), alpha = .4) +
   geom_text_repel(data = pvia_coords %>% arrange(desc(fatalities)) %>% head(), aes(x = lng, y = lat, label = city), size = 3) +
   theme_void() +
   labs(color = "Classification", size = "Fatalities") +
   ggtitle("Attacks in the United States (1948-2017)")
 
+# Map - incidents per capita
+classes <- c("Islamic extremism", "Left-wing", "Right-wing", "Separatist")
 
+incidents_percent <- initial_pvia_2 %>% 
+  filter(perpetrator_class %in% classes) %>% 
+  group_by(state) %>% 
+  count(fatalities) %>% 
+  group_by(state) %>% 
+  summarize(total = sum(n)) %>% 
+  inner_join(pop, by = "state") %>% 
+  mutate(incident_capita = total/pop_1980) %>% 
+  dplyr::select(state, incident_capita)
+  
+pvia_coords <- pvia_coords %>% 
+  inner_join(incidents_percent, by = "state") 
+
+temp <- pvia_coords %>% 
+  dplyr::select(-date) %>% 
+  unique() %>% 
+  group_by(state) %>% 
+  arrange(row_number(desc(incident_capita))) %>% 
+  head(30) 
+labels <- temp[c(1, 8, 12, 18, 19),]
+little_labels <- temp[c(9, 10:11, 13:17, 18, 21, 22, 24, 25, 27),]
+
+pvia_coords %>% 
+  filter(perpetrator_class %in% classes) %>% 
+  ggplot() +
+  geom_polygon(data = US, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3, color = "lightgray") + guides(fill = T) +
+  geom_point( aes(x = lng, y = lat, color = perpetrator_class, size = incident_capita), alpha = .4) +
+  geom_text_repel(data = labels, aes(x = lng, y = lat, label = city), size = 3) +
+  geom_text_repel(data = little_labels, aes(x = lng, y = lat, label = city), size = 2) +
+  theme_void() +
+  labs(color = "Classification", size = "Attacks per Capita") +
+  ggtitle("Attacks Per Capita (1948-2017)", subtitle = "Using 1980 population data")
+  
 
 # Bar plots
 # Bar plot of ideologies and deaths
-deaths_by_ideol <- pvia_coords %>% filter(!is.na(perpetrator_poli_leaning), !is.na(fatalities)) %>% group_by(perpetrator_poli_leaning) %>% summarize(all_deaths = sum(fatalities))
+deaths_by_ideol <- pvia_coords %>% filter(!is.na(perpetrator_class), !is.na(fatalities)) %>% group_by(perpetrator_class) %>% summarize(all_deaths = sum(fatalities))
 
-ggplot() +
-  geom_bar(data = pvia_coords %>% filter(!is.na(perpetrator_poli_leaning)) %>%  count(perpetrator_poli_leaning), 
-           aes(x = reorder(perpetrator_poli_leaning, -n), y = n), stat = "identity", fill = "lightskyblue3") + 
+pvia_coords %>% 
+  filter(perpetrator_class %in% classes) %>% 
+  ggplot() +
+  geom_bar(data = pvia_coords %>% filter(!is.na(perpetrator_class)) %>%  count(perpetrator_class), 
+           aes(x = reorder(perpetrator_class, -n), y = n), stat = "identity", fill = "lightskyblue3") + 
   geom_bar(data = deaths_by_ideol, 
-           aes(x = reorder(perpetrator_poli_leaning, -all_deaths), y = all_deaths, fill = all_deaths), stat = "identity", fill = "lightskyblue4") +
+           aes(x = reorder(perpetrator_class, -all_deaths), y = all_deaths, fill = all_deaths), stat = "identity", fill = "lightskyblue4") +
   theme_light() +
   ggtitle("Assassinations Committed by Political Leanings") +
   xlab("Perpetrator by Political Leaning") +
@@ -77,21 +115,22 @@ ggplot() +
 
 
 # Bar plot of deaths by target type
-deaths_by_group <- pvia_coords %>% filter(!is.na(target_type_1), !is.na(fatalities)) %>% group_by(target_type_1) %>% summarize(all_deaths = sum(fatalities))
+deaths_by_group <- pvia_coords %>% filter(!is.na(target_type_1), !is.na(fatalities), target_type_1 != "Police/Military") %>% group_by(target_type_1) %>% summarize(all_deaths = sum(fatalities))
 
 ggplot() +
-  geom_bar(data = pvia_coords %>% filter(!is.na(target_type_1)) %>%  count(target_type_1), 
-           aes(x = reorder(target_type_1, -n), y = n), stat = "identity", fill = "lightskyblue3", width = .5) + 
-  geom_bar(data = deaths_by_group, 
-           aes(x = reorder(target_type_1, -all_deaths), y = all_deaths, fill = all_deaths), stat = "identity", fill = "lightskyblue4", width = .5) +
+  geom_histogram(data = pvia_coords %>% filter(!is.na(target_type_1), target_type_1 != "Police/Military") %>%  count(target_type_1), 
+           aes(x = reorder(target_type_1, n), y = n), stat = "identity", fill = "lightskyblue3", binwidth = .3) + 
+  geom_histogram(data = deaths_by_group, 
+           aes(x = reorder(target_type_1, -all_deaths), y = all_deaths, fill = all_deaths), stat = "identity", fill = "lightskyblue4", binwidth = .3) +
   #theme_light() +
   ggtitle("Assassinations by Target") +
   xlab("Type of Target") +
   ylab("Number") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 6.5))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10)) +
+  coord_flip()
   
 # Success of attack based on target type
-deaths_by_group <- initial_pvia_2 %>% filter(!is.na(target_type_1), !is.na(fatalities)) %>% group_by(target_type_1) %>% summarize(all_deaths = sum(fatalities))
+deaths_by_group <- initial_pvia_2 %>% filter(!is.na(target_type_1), !is.na(fatalities), target_type_1 != "Police/Military") %>% group_by(target_type_1) %>% summarize(all_deaths = sum(fatalities))
 
 tot_attacks <- initial_pvia_2 %>% 
   filter(!is.na(target_type_1), !is.na(fatalities)) %>% 
@@ -114,29 +153,42 @@ deaths_by_group %>%
 
 # Time Series
 pvia_coords %>% 
-  filter(!is.na(target_type_1), !is.na(fatalities)) %>% 
+  mutate(year = as.integer(str_extract(date, "\\d{4}"))) %>% 
+  group_by(year) %>% 
+  count(fatalities) 
+  #filter(!is.na(target_type_1), !is.na(fatalities)) %>% 
  # count(target_type_1) %>% 
-  ggplot(aes(x = date, y = fatalities)) + 
-  geom_line(aes(group = perpetrator_poli_leaning, color = perpetrator_poli_leaning))
-  geom_smooth(aes(color= perpetrator_poli_leaning), se = F)
+  ggplot(aes(x = date, y = n)) + 
+  geom_line()
+  #geom_line(aes(group = perpetrator_class, color = perpetrator_class))
+  geom_smooth(se = F)
+  
+  
+pvia_coords %>% 
+  filter(!is.na(target_type_1), !is.na(fatalities), !is.na(perpetrator_class)) %>% 
+  ggplot(aes(x = date, y = fatalities)) +
+  #geom_smooth() +
+  geom_smooth(aes(group = perpetrator_class))
 
-# Instances by state
+# Raw instances by state
 initial_pvia_2 %>%
-    filter(!is.na(state)) %>% 
+    filter(!is.na(state), target_type_1 != "Police/Military") %>% 
     count(state) %>% 
     #group_by(state) %>% 
     #mutate()
     #summarize(total = sum(n))
-    ggplot(aes(x = reorder(state, -n), y = n)) + 
+    ggplot(aes(x = reorder(state, n), y = n)) + 
     geom_bar(stat = "identity", fill = "lightskyblue4") +
     xlab("State") +
     ylab("Count") +
-    ggtitle("Recored Instances by State") +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) 
-
+    ggtitle("Raw Counts of Instances by State") +
+   # theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) +
+    coord_flip()
+ggsave("attack_count_by_state.png")
 
 # Scatter plot of attacks over time by target type
 initial_pvia_2 %>%
+  filter(target_type_1 != "Police/Military") %>% 
   ggplot(., aes(x = date, y = fatalities, color = target_type_1)) + 
   geom_point() + 
   geom_jitter() + 
