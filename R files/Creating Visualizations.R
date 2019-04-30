6dd8acf98ecba67bc025ee7755375d6e2b4b43a6
Amanda McDermott <42814674+Glacieus@users.noreply.github.com>
2019-04-29 20:04:48 -0400

    updated creating vis.R

--- R files/Creating Visualizations.R
+++ R files/Creating Visualizations.R
@@ -10,7 +10,7 @@ library(gridExtra)
 library(ggthemes)
 library(usmap)
 library(kableExtra)
-library(reshape2)
+#library(reshape2)
 
 
 pvia_2 <- read_csv("pvia_2.csv")
@@ -96,16 +96,29 @@ plot_usmap(data = deaths_by_state,
   ggtitle("Attacks in United States (1948-2017)")
 
 # Incidents per capita
+state_list2 <- state_list[state_list != c("DC", "PR")]
 plot_usmap(data = pvia_2,
            values = "incidents_percapita",
-           lines = "darkblue") +
-  scale_fill_hue()
+           lines = "darkblue",
+           include = state_list2) +
+scale_fill_continuous(low = "lightcyan", high = "steelblue3", name = "Number of Attacks") +
+theme_void() +
+ggtitle("Attacks in United States Per Capita (1948-2017)")
+
+# DC attacks per capita
+plot_usmap(data = pvia_2,
+           values = "incidents_percapita",
+           lines = "darkblue",
+           include = "DC") +
+  scale_fill_continuous("navyblue", name = "") +
+  theme_void() +
+  ggtitle("District of Colombia")
 
 
 # Map  - locations ---------------------------------------------------------------
 (temp <- pvia_coords %>% arrange(desc(fatalities)))
 
-map1_lab <- temp[c(1:3, 5, 6, 11, 13, 15, 68),]
+map1_lab <- temp[c(1, 3, 4, 7, 8, 14),]
 
 ggplot() +
   geom_polygon(data = US, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3, color = "lightgray") + guides(fill = T) +
@@ -166,8 +179,8 @@ ggplot(deaths_by_ideol, aes(x = reorder(perpetrator_class, n), y = n, fill = Key
   coord_flip() +
   scale_fill_manual(values = c("lightskyblue3", "lightskyblue4"), labels = c("Total Attacks", "Total Kills")) +
   theme(legend.position = "bottom") +
-  ylim(0, 65) +
-  annotate("text", x = 1:4, y = c(16, 17, 30, 64), label = c("36.4%", "33.3%", "58.3%","88.3%")) +
+  ylim(0, 80) +
+  annotate("text", x = 1:4, y = c(16, 27, 27, 75), label = c("37.5%", "60%", "50%","89.7%")) +
   labs(caption = "Percentages shown represent the proportion of successful attacks")
 
 
@@ -198,7 +211,8 @@ deaths_by_group %>%
   coord_flip() +
   scale_fill_manual(values = c("lightskyblue3", "lightskyblue4"), labels = c("Total Attacks", "Total Kills")) +
   theme(legend.position = "bottom") +
-  annotate("text", x = 5:12, y = c(8, 10, 12, 18, 19, 20, 36, 41), label= c("40%","66.7%", "", "76.9%","80%", "26.7%", "56.2%", "73%")) +labs(caption = "Percentages shown represent the proportion of successful attacks")
+  ylim(0,45) +
+  annotate("text", x = 5:12, y = c(10, 10, 11, 17, 18, 20, 37, 43), label= c("57.1%","57.1%", "", "76.9%","80%", "37.5%", "60.6%", "85%")) +labs(caption = "Percentages shown represent the proportion of successful attacks")
 
 
 
@@ -227,29 +241,6 @@ deaths_by_state %>%
   scale_fill_manual(values = c("lightskyblue4", "lightskyblue3"), labels = c("Total Kills", "Total Attacks")) +
   theme(legend.position = "bottom")
 
-# tests
-groups_prop <- pvia_2 %>% 
-  filter(perpetrator_class %in% classes, target_type_1 != "Police/Military", !is.na(fatalities)) %>% 
-  count(target_type_1, perpetrator_class, fatalities) %>% 
-  #na.omit() %>% 
-  group_by(perpetrator_class) %>% 
-  mutate(total_incidents_perp = sum(n)) %>% 
-  mutate(total_deaths_perp = sum(fatalities * n)) %>% 
-  dplyr::select(-fatalities, -n) %>% 
-  unique() %>% 
-  mutate(percent_success_perp = total_deaths_perp/total_incidents_perp * 100) %>% 
-  transform(percent_success_perp = paste0(round(percent_success_perp, digits = 1), " %"))
-
-groups_prop %>% 
-  mutate(new = total_incidents_perp - total_deaths_perp) %>% 
-  select(-percent_success_perp, -total_incidents_perp) %>% 
-  gather(total_deaths_perp:new, key = Key, value = n) %>%
-  filter(Key == "new") %>% 
-  ggplot(aes(x = n, fill = target_type_1)) +
- # geom_point() +
-  facet_grid(perpetrator_class~target_type_1) +
-  geom_bar()
-  
 
 
 
@@ -258,8 +249,8 @@ targets_int <- c("Government", "Private Citizens", "Civil Rights Activists", "Re
                  "Political Party", "Abortion Related")
 # Assassination attacks and deaths over time
 pvia_2 %>% 
- #filter(perpetrator_class == c("Right-wing", "Left-wing")) %>% 
-  transform(perpetrator_class = factor(perpetrator_class, levels = c("Right-wing", "Left-wing", "Separatist", "Islamic extremism"))) %>% 
+ filter(perpetrator_class == c("Right-wing", "Left-wing", "Separatist")) %>% 
+  #transform(perpetrator_class = factor(perpetrator_class, levels = c("Right-wing", "Left-wing", "Separatist"))) %>% 
   mutate(year = as.integer(str_extract(date, "\\d{4}"))) %>% 
   filter(perpetrator_class %in% classes, target_type_1 %in% targets_int) %>% 
   count(year, fatalities, perpetrator_class, target_type_1) %>% 
@@ -271,13 +262,13 @@ pvia_2 %>%
   geom_point(alpha = .3)+
   geom_jitter(width = .4, alpha = .3) +
   #geom_smooth(se = F) 
-  geom_smooth(aes(group = perpetrator_class, color = perpetrator_class),  se = F, alpha = 0.8) +
-  #facet_grid(perpetrator_class ~ target_type_1) +
+  geom_smooth(aes(color = perpetrator_class), se = F, alpha = 0.8) +
+  facet_grid(perpetrator_class ~ .) +
   theme_light() +
   ylab("Attacks") +
   xlab("Year") +
   ggtitle("Attacks Over Time By Class") +
-  scale_color_discrete(labels = c("Left-Wing", "Right-Wing"), name = "Perpetrator Class") +
+  scale_color_discrete(labels = c("Left-Wing", "Right-Wing", "Separatist"), name = "Perpetrator Class") +
   theme(legend.position = "bottom", axis.text.x = element_text(angle = 45, hjust = 1, size = 8)) +
   scale_x_continuous(breaks = seq(1910, 2017, by = 5))
 
@@ -317,30 +308,3 @@ pvia_2 %>%
   ylab("Fatalities") +
   xlab("Date")
 
-
-
-
-
-
-
-# Success of attack based on target type
-
-
-# Time Series
-pvia_coords %>% 
-  mutate(year = as.integer(str_extract(date, "\\d{4}"))) %>% 
-  group_by(year) %>% 
-  count(fatalities) 
-#filter(!is.na(target_type_1), !is.na(fatalities)) %>% 
-# count(target_type_1) %>% 
-ggplot(aes(x = date, y = n)) + 
-  geom_line()
-#geom_line(aes(group = perpetrator_class, color = perpetrator_class))
-geom_smooth(se = F)
-
-
-pvia_coords %>% 
-  filter(!is.na(target_type_1), !is.na(fatalities), !is.na(perpetrator_class)) %>% 
-  ggplot(aes(x = date, y = fatalities)) +
-  #geom_smooth() +
-  geom_smooth(aes(group = perpetrator_class))
